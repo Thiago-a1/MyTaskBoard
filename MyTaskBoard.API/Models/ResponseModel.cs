036e04886ac55ee07b324764d750bc876e8867c8
Thiago Araujo <thiago.araujo.r11@gmail.com>
2024-09-03 20:05:44 -0300

    add: adionado model de response padrao.

---
+++ MyTaskBoard.API/Models/ResponseModel.cs
@@ -0,0 +1,8 @@
+﻿namespace MyTaskBoard.API.Models;
+
+public class ResponseModel<T>
+{
+    public List<T>? Data { get; set; }
+    public string Message { get; set; } = string.Empty;
+    public bool Status { get; set; } = false;
+}
